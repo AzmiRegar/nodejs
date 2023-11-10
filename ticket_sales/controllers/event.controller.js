d14e4271e82acb68e5f84c97939444dca96c2c7c
Rega <azmiregar8@gmail.com>
2023-11-10 22:48:31 +0700

    tiket_sales

--- ticket_sales/controllers/event.controller.js
+++ ticket_sales/controllers/event.controller.js
@@ -33,3 +33,108 @@ exports.findEvent = async (request, response) => {
         message: `All Events have been loaded`
     })
 }
+
+const upload = require(`./upload-image`).single(`image`)
+
+exports.addEvent = (request,response) => {
+    upload(request, response, async error => {
+        if (error) {
+            return response.json({ message: error})
+        }
+        if (!request.file) {
+            return response.json({ message: `Nothing to Upload`})
+        }
+
+        let newEvent = {
+            eventName: request.body.eventName,
+            eventDate: request.body.eventDate,
+            venue: request.body.venue,
+            price: request.body.price,
+            image: request.file.filename
+        }
+
+        eventModel.create(newEvent)
+        .then(result => {
+            return response.json({
+                success: true,
+                data: result,
+                message: `New event has been inserted`
+            })
+        })
+        .catch (error => {
+            return response.json({
+                success: false,
+                message: error.message
+            })
+        })
+    })
+}
+
+
+exports.updateEvent = async (request,response) => {
+    upload(request,response, async error => {
+        if (error) {
+            return response.json({ message: error})
+        }
+
+        let eventID = request.params.id
+        let dataEvent = {
+            eventName: request.body.eventName,
+            eventDate: request.body.eventDate,
+            venue: request.body.venue,
+            price: request.body.price
+        }
+
+        if (request.file) {
+            const selectedEvent = await eventModel.findOne({
+                where: {eventID:eventID}
+            })
+
+            const oldImage = selectedEvent.image
+            const pathImage = path.join(__dirname, `../image`, oldImage)
+            if(fs.existsSync(pathImage)) {
+                fs.unlink(pathImage, error => console.log(error))
+            }
+            dataEvent.image = request.file.filename  
+        }
+
+        eventModel.update(dataEvent, { where: { eventID: eventID }})
+            .then(result => {
+                return response.json({
+                    success: true,
+                    message: `Data event has been updated`
+                })
+            })
+            .catch(error => {
+                return response.json({
+                    success: false,
+                    message: error.message
+                })
+            })
+    })
+}
+
+
+exports.deleteEvent = async (request, response) => {
+    const eventID = request.params.id
+    const event = await eventModel.findOne({where: {eventID:eventID}})
+    const oldImage = event.image
+    const pathImage = path.join(__dirname, `../image`, oldImage)
+
+    if(fs.existsSync(pathImage)) {
+        fs.unlink(pathImage, error => console.log(error))
+    }
+    eventModel.destroy({ where: {eventID: eventID}})
+        .then(result => {
+            return response.json({
+                success: true,
+                message: `Data event has been deleted`
+            })
+        })
+        .catch(error => {
+            return response.json({
+                success: false,
+                message: error.message
+            })
+        })
+}
--- ticket_sales/index.js
+++ ticket_sales/index.js
@@ -4,8 +4,15 @@ const PORT = 8000
 
 const cors = require(`cors`)
 app.use(cors())
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
 const userRoute = require(`./routes/user.route`)
 app.use(`/user`, userRoute)
+
+const eventRoute = require(`./routes/event.route`)
+app.use(`/event`, eventRoute)
+app.use(express.static(__dirname))
 
 app.listen(PORT, () => {
     console.log(`Server of Ticket Sales run on port ${PORT}`)
