025af1442e02823757bf325e2e888cc954c69ff0
Rega <azmiregar8@gmail.com>
2024-01-04 18:27:36 +0700

    tiket_sales

--- ticket_sales/index.js
+++ ticket_sales/index.js
@@ -7,13 +7,18 @@ app.use(cors())
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
+
+//user
 const userRoute = require(`./routes/user.route`)
 app.use(`/user`, userRoute)
 
+//event
 const eventRoute = require(`./routes/event.route`)
 app.use(`/event`, eventRoute)
 app.use(express.static(__dirname))
 
+
+//port
 app.listen(PORT, () => {
     console.log(`Server of Ticket Sales run on port ${PORT}`)
 })
